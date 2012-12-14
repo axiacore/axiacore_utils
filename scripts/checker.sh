9facd5df7b89cb58899e401cf8e4725518273ead
Juan Pablo Romero Bernal <juan.romero@axiacore.com>
2012-12-14 14:23:05 -0500

    Initial commit, with a simple script

---
+++ scripts/checker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+################################################
+# Script to check pep8 and pylint standards
+# Author: AxiaCore S.A.S
+# Notes:
+# You must run this script in active virtualenv
+################################################
+
+echo -e "Running pep8 checking .....\n"
+git status | grep 'py$' | grep 'modified' | awk '{print $3}' | xargs pep8
+if [ $? -eq 0 ] ; then echo -e "Not pep8 errors found"; fi
+
+echo -e "Running pylint checking .....\n"
+git status | grep 'py$' | grep 'modified' | awk '{print $3}' | xargs pylint --rcfile=pylint.rc -r n
+if [ $? -eq 0 ] ; then echo -e "Not pylint errors found"; fi
