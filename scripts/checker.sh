833dda1a56497c5d530ad3a96af584a59e013299
Juan Pablo Romero Bernal <juan.romero@axiacore.com>
2013-01-02 09:53:51 -0500

    Minor change to support the new files for pep8 and pylint checking

--- scripts/checker.sh
+++ scripts/checker.sh
@@ -9,8 +9,12 @@
 
 echo -e "Running pep8 checking .....\n"
 git status | grep 'py$' | grep 'modified' | awk '{print $3}' | xargs pep8
-if [ $? -eq 0 ] ; then echo -e "Not pep8 errors found"; fi
+if [ $? -eq 0 ] ; then echo -e "Not pep8 errors found in modified files"; fi
+git status | grep 'py$' | grep -v 'modified' | awk '{print $2}' | xargs pep8
+if [ $? -eq 0 ] ; then echo -e "Not pep8 errors found in new files"; fi
 
 echo -e "Running pylint checking .....\n"
 git status | grep 'py$' | grep 'modified' | awk '{print $3}' | xargs pylint --rcfile=pylint.rc -r n
-if [ $? -eq 0 ] ; then echo -e "Not pylint errors found"; fi
+if [ $? -eq 0 ] ; then echo -e "Not pylint errors found in modified files"; fi
+git status | grep 'py$' | grep -v 'modified' | awk '{print $2}' | xargs pylint --rcfile=pylint.rc -r n
+if [ $? -eq 0 ] ; then echo -e "Not pylint errors found in new files"; fi
