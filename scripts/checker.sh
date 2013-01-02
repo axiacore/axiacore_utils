1128e026134f66c36724ab9962e3c9886de8a0b4
Juan Pablo Romero Bernal <juan.romero@axiacore.com>
2013-01-02 17:10:22 -0500

    Minor fix

--- scripts/checker.sh
+++ scripts/checker.sh
@@ -4,17 +4,19 @@
 # Author: AxiaCore S.A.S
 # Notes:
 # You must run this script in active virtualenv
-# and in the root of project
+# and from the root of project
 ################################################
 
-echo -e "Running pep8 checking .....\n"
+echo -e "Running pep8 checking in modified files .....\n"
 git status | grep 'py$' | grep 'modified' | awk '{print $3}' | xargs pep8
 if [ $? -eq 0 ] ; then echo -e "Not pep8 errors found in modified files"; fi
+echo -e "Running pep8 checking in new files .....\n"
 git status | grep 'py$' | grep -v 'modified' | awk '{print $2}' | xargs pep8
 if [ $? -eq 0 ] ; then echo -e "Not pep8 errors found in new files"; fi
 
-echo -e "Running pylint checking .....\n"
+echo -e "Running pylint checking in modified files .....\n"
 git status | grep 'py$' | grep 'modified' | awk '{print $3}' | xargs pylint --rcfile=pylint.rc -r n
 if [ $? -eq 0 ] ; then echo -e "Not pylint errors found in modified files"; fi
+echo -e "Running pylint checking in new files .....\n"
 git status | grep 'py$' | grep -v 'modified' | awk '{print $2}' | xargs pylint --rcfile=pylint.rc -r n
 if [ $? -eq 0 ] ; then echo -e "Not pylint errors found in new files"; fi
