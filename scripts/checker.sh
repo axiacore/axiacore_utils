0dc44c767497ccbb374916b7cf95da7513c125b1
Juan Pablo Romero Bernal <juan.romero@axiacore.com>
2013-03-19 23:10:20 -0500

    Adding support to jshint checking and removing several unnecesary print lines

--- scripts/checker.sh
+++ scripts/checker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ################################################
-# Script to check pep8 and pylint standards
+# Script to check pep8, pylint and jshint standards
 # Author: AxiaCore S.A.S
 # Notes:
 # You must run this script in active virtualenv
@@ -15,49 +15,33 @@ NEW_EXPR='^A|^ A'
 GIT_COMMAND='git status --porcelain'
 PEP8_COMMAND='pep8'
 PYLINT_COMMAND='pylint --rcfile=pylint.rc -r n'
+JSHINT_COMMAND='jshint'
 PRINT_COMMAND='grep -n -H --colour=auto print'
 
 # pep8 checking
-
-echo -e "Running pep8 checking in modified files .....\n"
+echo -e "Running pep8 checking .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${MODIFIED_EXPR}" | awk '{print $2}' | xargs ${PEP8_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not pep8 errors found in modified files"; fi
-echo -e "Running pep8 checking in new files .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${UNTRACKED_EXPR}" | awk '{print $2}' | xargs ${PEP8_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not pep8 errors found in new files"; fi
-echo -e "Running pep8 checking in renamed files .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${RENAMED_EXPR}" | awk '{print $4}' | xargs ${PEP8_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not pep8 errors found in renamed files"; fi
-echo -e "Running pep8 checking in new files .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${NEW_EXPR}" | awk '{print $2}' | xargs ${PEP8_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not pep8 errors found in new files"; fi
 
 # pylint checking
-
-echo -e "Running pylint checking in modified files .....\n"
+echo -e "Running pylint checking .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${MODIFIED_EXPR}" | awk '{print $2}' | xargs ${PYLINT_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not pylint errors found in modified files"; fi
-echo -e "Running pylint checking in new files .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${UNTRACKED_EXPR}" | awk '{print $2}' | xargs ${PYLINT_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not pylint errors found in new files"; fi
-echo -e "Running pylint checking in renamed files .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${RENAMED_EXPR}" | awk '{print $4}' | xargs ${PYLINT_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not pylint errors found in renamed files"; fi
-echo -e "Running pylint checking in new files .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${NEW_EXPR}" | awk '{print $2}' | xargs ${PYLINT_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not pylint errors found in new files"; fi
 
 # print checking
-
-echo -e "Running checking for print calls in modified files .....\n"
+echo -e "Running checking for print calls .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${MODIFIED_EXPR}" | awk '{print $2}' | xargs ${PRINT_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not print calls found in modified files"; fi
-echo -e "Running checking for print calls in new files .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${UNTRACKED_EXPR}" | awk '{print $2}' | xargs ${PRINT_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not print calls found in new files"; fi
-echo -e "Running checking for print calls in renamed files .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${RENAMED_EXPR}" | awk '{print $4}' | xargs ${PRINT_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not print calls found in renamed files"; fi
-echo -e "Running checking for print calls in new files .....\n"
 ${GIT_COMMAND} | grep 'py$' | egrep "${NEW_EXPR}" | awk '{print $2}' | xargs ${PRINT_COMMAND}
-if [ $? -eq 0 ] ; then echo -e "Not print calls found in new files"; fi
+
+# jshint checking
+echo -e "Running jshint checking .....\n"
+${GIT_COMMAND} | grep 'js$' | egrep "${MODIFIED_EXPR}" | awk '{print $2}' | xargs ${JSHINT_COMMAND}
+${GIT_COMMAND} | grep 'js$' | egrep "${UNTRACKED_EXPR}" | awk '{print $2}' | xargs ${JSHINT_COMMAND}
+${GIT_COMMAND} | grep 'js$' | egrep "${RENAMED_EXPR}" | awk '{print $4}' | xargs ${JSHINT_COMMAND}
+${GIT_COMMAND} | grep 'js$' | egrep "${NEW_EXPR}" | awk '{print $2}' | xargs ${JSHINT_COMMAND}
